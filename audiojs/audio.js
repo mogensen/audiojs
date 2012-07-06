2f57ba4fb532084b61722f41e05b3f72c3b03b9e
dotzero <mail@dotzero.ru>
2012-07-06 11:08:27 +0400

    Fix wrong call initial volume

--- audiojs/audio.js
+++ audiojs/audio.js
@@ -275,7 +275,7 @@
       if (!s.useFlash || (s.useFlash && s.hasFlash)) this.attachEvents(audio.wrapper, audio);
 
       // Set initial volume to the new audiojs instance.
-      audio.settings.volume.call(audio, s.initialVolume);
+      audio.setVolume.call(audio, s.initialVolume);
 
       // Store the newly-created `audiojs` instance.
       this.instances[id] = audio;
